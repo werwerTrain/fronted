0698fcf19e1490eac9f2b47ae2422c3a51c76cb9
李佳璐 <12439279+llijialu@user.noreply.gitee.com>
2024-08-24 11:26:24 +0800

    a

--- frontend/src/api/request.js
+++ frontend/src/api/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const api = axios.create(
   {
-    baseURL: "http://localhost:8082", //这里配置的是后端服务提供的接口
+    baseURL: "http://localhost:30001", //这里配置的是后端服务提供的接口
   }
 );
 export default api;
